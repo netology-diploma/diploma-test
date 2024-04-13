662e1b5b44ac5f21d9bd495127e73e66836cc666
Alexey Tasenko <fenixad@gmail.com>
2024-04-13 22:26:19 +0300

    fix:add access to master

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,3 +88,23 @@ resource "yandex_vpc_security_group" "k8s-public-services" {
     to_port           = 65535
   }
 }
+
+resource "yandex_vpc_security_group" "k8s-master-whitelist" {
+  name        = "k8s-master-whitelist"
+  description = "Правила группы разрешают доступ к API Kubernetes из интернета. Примените правила только к кластеру."
+  network_id  = yandex_vpc_network.default.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Правило разрешает подключение к API Kubernetes через порт 6443 из указанной сети."
+    v4_cidr_blocks = ["46.188.123.160/32"]
+    port           = 6443
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Правило разрешает подключение к API Kubernetes через порт 443 из указанной сети."
+    v4_cidr_blocks = ["46.188.123.160/32"]
+    port           = 443
+  }
+}
