7b53f86b37ebb9681f36de1fafaa1eea2679a548
Aleksei Tasenko <a@tasenko.ru>
2024-05-10 11:40:49 +0300

    init:k8s-cluster

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-## Дипломный практикум в Yandex.Cloud  
-### 1. Подготовить облачную инфраструктуру на базе облачного провайдера Яндекс.Облако.  
-- Сервисный аккаунт создается в файле [sa-rbac-keys.tf](terraform/sa-rbac-keys.tf), там же ему назначаются рекомендованные роли и создается симметричный ключ для шифрования ресурсов.  
-- Бэкенд для Terraform подготовил в Terraform Cloud. Описан в файле [provider.tf](terraform/provider.tf), там же указаны переменные с секретами для доступа к облаку. Сами секреты сохранены в безопасных переменных Terraform Cloud.  
-- VPC с подсетями во всех зонах доступности создаются в [network.tf](terraform/network.tf), там же группы безопасности для кластера.  
-- В pull-request еще до слияния с веткой main по которой настроен запуск Run в Terraform Cloud сразу можно просмотреть plan и подправить ошибки.  
-![not done :(](img_d/diploma_01.png)  
-- Если все проверки прошли успешно, "мержим" в main.  
-![complete](img_d/diploma_02.png)  
-- Результат - запуск Run в Terraform Cloud и создание объектов в Yandex Cloud.  
-![plan applied](img_d/diploma_03.png)  
-![infra created](img_d/diploma_04.png)  
+### Задание 1. Yandex Cloud.  
+#### 1. Настроить с помощью Terraform кластер баз данных MySQL.  
+
+
+
+#### 2. Настроить с помощью Terraform кластер Kubernetes.  
--- terraform/cluster_k8s.tf
+++ terraform/cluster_k8s.tf
@@ -1,22 +1,49 @@
 resource "yandex_kubernetes_cluster" "diploma" {
   name = "yandex-k8s-${var.environment}"
   network_id = yandex_vpc_network.kuber-network.id
+
   master {
+    regional {
+      region = var.zone_1a
+      location {
+        zone      = yandex_vpc_subnet.public-subnet-1a.zone
+        subnet_id = yandex_vpc_subnet.public-subnet-1a.id
+      }
+      location {
+        zone      = yandex_vpc_subnet.public-subnet-1b.zone
+        subnet_id = yandex_vpc_subnet.public-subnet-1b.id
+      }
+      location {
+        zone      = yandex_vpc_subnet.public-subnet-1d.zone
+        subnet_id = yandex_vpc_subnet.public-subnet-1d.id
+      }
+    }
+
     public_ip = true
-    master_location {
-      zone      = yandex_vpc_subnet.public-subnet-1a.zone
-      subnet_id = yandex_vpc_subnet.public-subnet-1a.id
-   }
-    security_group_ids = [yandex_vpc_security_group.k8s-master-whitelist.id]
+    security_group_ids = [
+      yandex_vpc_security_group.k8s-master-whitelist.id,
+      yandex_vpc_security_group.k8s-public-services.id
+    ]
+
+    maintenance_policy {
+      auto_upgrade = true
+      maintenance_window {
+        day        = "monday"
+        start_time = "23:00"
+        duration   = "3h"
+      }
+    }
   }
+
   service_account_id      = yandex_iam_service_account.cluster-sa.id
   node_service_account_id = yandex_iam_service_account.cluster-sa.id
-    depends_on = [
-      yandex_resourcemanager_folder_iam_member.k8s-clusters-agent,
-      yandex_resourcemanager_folder_iam_member.vpc-public-admin,
-      yandex_resourcemanager_folder_iam_member.images-puller,
-      yandex_resourcemanager_folder_iam_member.encrypterDecrypter
+  depends_on = [
+    yandex_resourcemanager_folder_iam_member.k8s-clusters-agent,
+    yandex_resourcemanager_folder_iam_member.vpc-public-admin,
+    yandex_resourcemanager_folder_iam_member.images-puller,
+    yandex_resourcemanager_folder_iam_member.encrypterDecrypter
   ]
+
   kms_provider {
     key_id = yandex_kms_symmetric_key.kms-key.id
   }
@@ -25,19 +52,45 @@ resource "yandex_kubernetes_cluster" "diploma" {
 resource "yandex_kubernetes_node_group" "diploma-nodes" {
   cluster_id    = yandex_kubernetes_cluster.diploma.id
   name          = "nodes-diploma-${var.environment}"
+
   instance_template {
     name        = "${var.environment}-{instance.short_id}"
     platform_id = "standard-v2"
+
     resources {
       memory        = var.node_resources.memory
       cores         = var.node_resources.cores
       core_fraction = var.node_resources.core_fraction
     }
+
+    boot_disk {
+      type = "network-hdd"
+      size = 64
+    }
+
+    network_interface {
+      nat                = false
+      subnet_ids         = [
+        yandex_vpc_subnet.public-subnet-1a.id,
+        yandex_vpc_subnet.public-subnet-1b.id,
+        yandex_vpc_subnet.public-subnet-1d.id
+      ]
+      security_group_ids = [
+        yandex_vpc_security_group.k8s-public-services.id
+      ]
+    }
+
     network_acceleration_type = "standard"
+
+    scheduling_policy {
+      preemptible = alltrue()
+    }
+
     container_runtime {
       type = "containerd"
     }
   }
+
   scale_policy {
     auto_scale {
       initial = 1
