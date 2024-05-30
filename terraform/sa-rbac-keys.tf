9fed28857344a26e863117735a776dd042a2f251
Aleksei Tasenko <a@tasenko.ru>
2024-05-30 13:24:49 +0300

    fix:change gw desc

--- terraform/sa-rbac-keys.tf
+++ terraform/sa-rbac-keys.tf
@@ -20,6 +20,12 @@ resource "yandex_resourcemanager_folder_iam_member" "k8s-clusters-agent" {
  member    = "serviceAccount:${yandex_iam_service_account.cluster-sa.id}"
 }
 
+resource "yandex_resourcemanager_folder_iam_member" "k8s-cluster-api-editor" {
+ folder_id = var.folder_id
+ role      = "k8s.cluster-api.editor"
+ member    = "serviceAccount:${yandex_iam_service_account.cluster-sa.id}"
+}
+
 resource "yandex_resourcemanager_folder_iam_member" "vpc-public-admin" {
  folder_id = var.folder_id
  role      = "vpc.publicAdmin"
