ee63b7e48c828e840b6bafb1728d6badab8cd38a
Aleksei Tasenko <a@tasenko.ru>
2024-06-26 12:38:32 +0300

    fix:list inside map

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -45,28 +45,28 @@ variable "subnets" {
   type    = map(object({
     name           = string
     zone           = string
-    v4_cidr_blocks = string
+    v4_cidr_blocks = list(string)
   }))
   default = {
     public-subnet-1a = {
       name           = "public-subnet-1a",
       zone           = "ru-central1-a",
-      v4_cidr_blocks = "10.0.11.0/24"
+      v4_cidr_blocks = ["10.0.11.0/24"]
     }
     public-subnet-1b = {
       name           = "public-subnet-1b",
       zone           = "ru-central1-b",
-      v4_cidr_blocks = "10.0.12.0/24"
+      v4_cidr_blocks = ["10.0.12.0/24"]
     }
     public-subnet-1d = {
       name           = "public-subnet-1d",
       zone           = "ru-central1-d",
-      v4_cidr_blocks = "10.0.13.0/24"
+      v4_cidr_blocks = ["10.0.13.0/24"]
     }
     public-subnet-1c = {
       name           = "public-subnet-1c",
       zone           = "ru-central1-c",
-      v4_cidr_blocks = "10.0.14.0/24"
+      v4_cidr_blocks = ["10.0.14.0/24"]
     }
   }
 }
