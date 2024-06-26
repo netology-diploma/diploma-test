ec1c36ac217c3c73c3c96d59d2899a2dd1cd12ff
Aleksei Tasenko <a@tasenko.ru>
2024-06-26 14:17:49 +0300

    looks like done

--- terraform/network.tf
+++ terraform/network.tf
@@ -32,45 +32,28 @@ resource "yandex_vpc_security_group" "k8s-public-services" {
   name        = "k8s-public-${var.environment}"
   description = "Правила группы разрешают подключение к сервисам из интернета. Примените правила только для групп узлов."
   network_id  = yandex_vpc_network.kuber-network.id
-  ingress {
-    protocol          = "TCP"
-    description       = "Правило разрешает проверки доступности с диапазона адресов балансировщика нагрузки. Нужно для работы отказоустойчивого кластера Managed Service for Kubernetes и сервисов балансировщика."
-    predefined_target = "loadbalancer_healthchecks"
-    from_port         = 0
-    to_port           = 65535
+
+  dynamic "ingress" {
+    for_each = var.security_group_ingress_k8s_public
+    content {
+      protocol        = lookup(ingress.value, "protocol", null)
+      description     = lookup(ingress.value, "description", null)
+      port            = lookup(ingress.value, "port", null)
+      from_port       = lookup(ingress.value, "from_port", null)
+      to_port         = lookup(ingress.value, "to_port", null)
+      v4_cidr_blocks  = lookup(ingress.value, "v4_cidr_blocks", null)
+    }
   }
-  ingress {
-    protocol          = "ANY"
-    description       = "Правило разрешает взаимодействие мастер-узел и узел-узел внутри группы безопасности."
-    predefined_target = "self_security_group"
-    from_port         = 0
-    to_port           = 65535
-  }
-  ingress {
-    protocol          = "ANY"
-    description       = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера Managed Service for Kubernetes и сервисов."
-    v4_cidr_blocks    = concat(yandex_vpc_subnet.subnets[*].v4_cidr_blocks)
-    from_port         = 0
-    to_port           = 65535
-  }
-  ingress {
-    protocol          = "ICMP"
-    description       = "Правило разрешает отладочные ICMP-пакеты из внутренних подсетей."
-    v4_cidr_blocks    = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
-  }
-  ingress {
-    protocol          = "TCP"
-    description       = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
-    v4_cidr_blocks    = ["0.0.0.0/0"]
-    from_port         = 30000
-    to_port           = 32767
-  }
-  egress {
-    protocol          = "ANY"
-    description       = "Правило разрешает весь исходящий трафик. Узлы могут связаться с Yandex Container Registry, Yandex Object Storage, Docker Hub и т. д."
-    v4_cidr_blocks    = ["0.0.0.0/0"]
-    from_port         = 0
-    to_port           = 65535
+  dynamic "egress" {
+    for_each = var.security_group_egress_k8s_public
+    content {
+      protocol       = lookup(egress.value, "protocol", null)
+      description    = lookup(egress.value, "description", null)
+      port           = lookup(egress.value, "port", null)
+      from_port      = lookup(egress.value, "from_port", null)
+      to_port        = lookup(egress.value, "to_port", null)
+      v4_cidr_blocks = lookup(egress.value, "v4_cidr_blocks", null)
+    }
   }
 }
 
@@ -79,17 +62,26 @@ resource "yandex_vpc_security_group" "k8s-master-whitelist" {
   description = "Правила группы разрешают доступ к API Kubernetes из интернета. Примените правила только к кластеру."
   network_id  = yandex_vpc_network.kuber-network.id
 
-  ingress {
-    protocol       = "TCP"
-    description    = "Правило разрешает подключение к API Kubernetes через порт 6443 из указанной сети."
-    v4_cidr_blocks = var.client_network
-    port           = 6443
+  dynamic "ingress" {
+    for_each = var.security_group_ingress_k8s_master
+    content {
+      protocol       = lookup(ingress.value, "protocol", null)
+      description    = lookup(ingress.value, "description", null)
+      port           = lookup(ingress.value, "port", null)
+      from_port      = lookup(ingress.value, "from_port", null)
+      to_port        = lookup(ingress.value, "to_port", null)
+      v4_cidr_blocks = lookup(ingress.value, "v4_cidr_blocks", null)
+    }
   }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "Правило разрешает подключение к API Kubernetes через порт 443 из указанной сети."
-    v4_cidr_blocks = var.client_network
-    port           = 443
+  dynamic "egress" {
+    for_each = var.security_group_egress_k8s_master
+    content {
+      protocol       = lookup(egress.value, "protocol", null)
+      description    = lookup(egress.value, "description", null)
+      port           = lookup(egress.value, "port", null)
+      from_port      = lookup(egress.value, "from_port", null)
+      to_port        = lookup(egress.value, "to_port", null)
+      v4_cidr_blocks = lookup(egress.value, "v4_cidr_blocks", null)
+    }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -71,15 +71,6 @@ variable "subnets" {
   }
 }
 
-variable "client_network" {
-  type    = list(string)
-  default = [
-    "0.0.0.0/0",
-    "46.188.123.160/32",
-    "164.90.169.22/32"
-  ]
-}
-
 variable "region" {
   type    = string
   default = "ru-central1"
@@ -102,6 +93,115 @@ variable "zone_1c" {
 variable "zone_1d" {
   type    = string
   default = "ru-central1-d"
+}
+
+variable "security_group_ingress_k8s_public" {
+  type = list(object(
+    {
+      protocol          = string
+      description       = string
+      v4_cidr_blocks    = optional(list(string))
+      port              = optional(number)
+      from_port         = optional(number)
+      to_port           = optional(number)
+      predefined_target = optional(string)
+      }))
+  default = [
+    {
+      protocol          = "TCP"
+      description       = "Правило разрешает проверки доступности с диапазона адресов балансировщика нагрузки. Нужно для работы отказоустойчивого кластера Managed Service for Kubernetes и сервисов балансировщика."
+      predefined_target = "loadbalancer_healthchecks"
+      from_port         = 0
+      to_port           = 65535
+    },
+    {
+      protocol          = "ANY"
+      description       = "Правило разрешает взаимодействие мастер-узел и узел-узел внутри группы безопасности."
+      predefined_target = "self_security_group"
+      from_port         = 0
+      to_port           = 65535
+    },
+    {
+      protocol          = "ANY"
+      description       = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера Managed Service for Kubernetes и сервисов."
+      v4_cidr_blocks    = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24", "10.0.14.0/24"]
+      from_port         = 0
+      to_port           = 65535
+    },
+    {
+      protocol          = "ICMP"
+      description       = "Правило разрешает отладочные ICMP-пакеты из внутренних подсетей."
+      v4_cidr_blocks    = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    },
+    {
+      protocol          = "TCP"
+      description       = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
+      v4_cidr_blocks    = ["0.0.0.0/0"]
+      from_port         = 30000
+      to_port           = 32767
+    }
+  ]
+}
+
+variable "security_group_egress_k8s_public" {
+  type = list(object(
+    {
+      protocol          = string
+      description       = string
+      v4_cidr_blocks    = list(string)
+      port              = optional(number)
+      from_port         = optional(number)
+      to_port           = optional(number)
+      }))
+  default = [
+    {
+      protocol          = "ANY"
+      description       = "Правило разрешает весь исходящий трафик. Узлы могут связаться с Yandex Container Registry, Yandex Object Storage, Docker Hub и т. д."
+      v4_cidr_blocks    = ["0.0.0.0/0"]
+      from_port         = 0
+      to_port           = 65535
+    }
+  ]
+}
+
+variable "security_group_ingress_k8s_master" {
+  type = list(object(
+    {
+      protocol          = string
+      description       = string
+      v4_cidr_blocks    = optional(list(string))
+      port              = optional(number)
+      from_port         = optional(number)
+      to_port           = optional(number)
+      predefined_target = optional(string)
+      }))
+  default = [
+    {
+      protocol       = "TCP"
+      description    = "Правило разрешает подключение к API Kubernetes через порт 6443 из указанной сети."
+      v4_cidr_blocks = ["0.0.0.0/0"]
+      port           = 6443
+    },
+    {
+      protocol       = "TCP"
+      description    = "Правило разрешает подключение к API Kubernetes через порт 443 из указанной сети."
+      v4_cidr_blocks = ["0.0.0.0/0"]
+      port           = 443
+    }
+  ]
+}
+
+variable "security_group_egress_k8s_master" {
+  type = list(object(
+    {
+      protocol          = string
+      description       = string
+      v4_cidr_blocks    = list(string)
+      port              = optional(number)
+      from_port         = optional(number)
+      to_port           = optional(number)
+      }))
+  default = []
 }
 
 # Kubernetes cluster-related variables
